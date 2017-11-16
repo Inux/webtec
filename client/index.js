93b5614d92253786e5c11722e85b4698f0c48fc9
inux <inux.steve@gmail.com>
2017-11-16 02:06:13 +0100

    save actual page in cookie works

--- client/index.js
+++ client/index.js
@@ -1,14 +1,44 @@
 const jq = require("jquery");
-jq("#content").load("home.html");
+var qk = require('js-cookie');
+var content = "#content";
 
-jq("#navHome").click(function() {
-    jq("#content").load("home.html");
-});
+const pagesConfig = [
+    {
+        menu: '#navHome', 
+        page: 'home.html'
+    },
+    {
+        menu: '#navCanvas', 
+        page: 'canvas.html'
+    },
+    {
+        menu: '#navFormular', 
+        page: 'formular.html'
+    },
+    {
+        menu: '#navStyleSwitcher', 
+        page: 'styleswitcher.html'
+    }
+];
 
-jq("#navCanvas").click(function() {
-    jq("#content").load("canvas.html");
-});
+//Check if cookie is available 
+//if available change to saved page
+//if NOT navigate to home.html
+const pageck = qk.get('page');
+if(pageck != '' && pageck != null) {
+    jq(content).load(pageck);
+} else {
+    jq(content).load('home.html');
+}
 
-jq("#navFormular").click(function() {
-    jq("#content").load("formular.html");
-});
+//Add click listeners to menu (based on pagesConfig)
+for (var i = 0; i < pagesConfig.length; i++) {
+    let page = pagesConfig[i];
+    jq(page.menu).click(
+        function() {
+            qk.set('page', page.page, { expires: 7 });
+            jq(content).load(page.page);
+        }
+    );
+}
+
