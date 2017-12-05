c7556f2620d5e00b390aac699ec77088ddc58616
inux <inux.steve@gmail.com>
2017-12-05 15:54:53 +0100

    some style fixes and basic styleswitcher

---
+++ client/pages/styleswitcher/styleswitcher.js
@@ -0,0 +1,68 @@
+const jq = require("jquery");
+var qk = require('js-cookie');
+var content = "#styleSwitcher";
+
+const styleOrder = [
+    "--color-main",
+    "--color-background",
+    "--color-borders"
+];
+
+const styleConfig = {
+    dark: [
+        "#cdcdcd",
+        "rgba(23,23,23,0)",
+        "#ccc"
+    ],
+    light: [
+        "#232323",
+        "rgba(205,205,205,1)",
+        "#333"
+    ]
+}
+
+var setStyle = function(style) {
+    for(var i = 0; i < style.length; i++) {
+        jq("body").get(0).style.setProperty(
+            styleOrder[i],
+            style[i]
+        );
+    }
+}
+
+//Check if cookie is available 
+//if available change to saved style
+//if NOT take default
+const style = qk.get('style');
+console.log("cookie style value: " + style); 
+if(style != '' && style != null) {
+    if(style == "dark") {
+        setStyle(styleConfig.dark);
+        jq("#darkRadioBtn").prop("checked", true);
+    } else {
+        setStyle(styleConfig.light);
+        jq("#lightRadioBtn").prop("checked", true);
+    }
+} else {
+    setStyle(styleConfig.light);
+    jq("#lightRadioBtn").prop("checked", true);
+}
+
+jq("#darkRadioBtn").click(
+    function() {
+        jq("#darkRadioBtn").prop("checked", true);
+        qk.set('style', 'dark');
+        setStyle(styleConfig.dark);
+        console.log("pressed dark radio button");
+    }
+)
+
+jq("#lightRadioBtn").click(
+    function() {
+        jq("#lightRadioBtn").prop("checked", true);
+        qk.set('style', 'light');
+        setStyle(styleConfig.light);
+        console.log("pressed light radio button");
+    }
+)
+
